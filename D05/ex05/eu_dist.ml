c64240afb9b4c021a361b953a22e8e0ac015e3c5
Celia CORSIN <ccorsin@student.42.fr>
2020-01-21 14:56:08 +0100

    D05 ex05

--- D05/ex05/eu_dist.ml
+++ D05/ex05/eu_dist.ml
@@ -1,9 +1,17 @@
 let eu_dist (a:float array) (b:float array) =
-  let rec sum_square a b i acc =
-    if i = Array.length a then
+  let rec sum_square i acc =
+    if i = (Array.length a) then
       acc
     else
-      acc + ((a.(i) - b.(i)) ** 2);
-      sum_square a b (i + 1) acc
+      sum_square (i + 1) (acc +. ((a.(i) -. b.(i)) ** 2.))
   in
-  sum_square a b 1 0
+  sqrt (sum_square 0 0.)
+let () =
+  let a = [|1.; 1.; 1.; 1.|] in
+  let b = [|2.; 2.; 2.; 2.|] in
+  print_float (eu_dist a b);
+  print_endline "";
+  print_float (eu_dist [|4.5; 1.0; (-2.5); 0.4|] [|2.; 3.; 12.; (-42.)|]);
+  print_endline "";
+  print_float (eu_dist [|1.; (-2.)|] [| 4.; 2.|]);
+  print_endline "";
