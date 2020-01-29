b50550154028e49924299e01cb81598e59659c2b
Celia CORSIN <ccorsin@student.42.fr>
2020-01-29 19:38:12 +0100

    Fix ex02

--- D08/ex02/main.ml
+++ D08/ex02/main.ml
@@ -3,10 +3,14 @@ let main () =
   print_endline m1#to_string ;
   let m2 = new Alkane.ethane in
   print_endline m2#to_string ;
+  let m2_bis = new Alkane.ethane in
+  print_endline m2#to_string ;
   let m4 = new Alkane.butane in
   print_endline m4#to_string ;
   let m8 = new Alkane.octane in
-  print_endline m8#to_string
+  print_endline m8#to_string ;
+  print_endline (string_of_bool (m2#equals ((m2_bis :> Alkane.alkane) :> Molecule.molecule))) ;
+	print_endline (string_of_bool (m2#equals ((m8  :> Alkane.alkane) :> Molecule.molecule)))
 
 let () =
   main ()
